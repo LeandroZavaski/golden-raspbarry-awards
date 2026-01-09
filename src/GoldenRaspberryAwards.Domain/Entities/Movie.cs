533e200694ff43bad446934e125019ded8ecd855
Leandro Cezar Zavaski <leandro_zavaski@hotmail.com>
2026-01-09 17:26:16 -0300

    feat(domain): add Movie entity
    - Define Movie entity with Year, Title, Studios, Producers, Winner properties

---
+++ src/GoldenRaspberryAwards.Domain/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace GoldenRaspberryAwards.Domain.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Studios { get; set; } = string.Empty;
+        public string Producers { get; set; } = string.Empty;
+        public bool Winner { get; set; }
+    }
+}
