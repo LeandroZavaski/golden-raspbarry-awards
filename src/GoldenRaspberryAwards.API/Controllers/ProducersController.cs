b22e7ef671db01a261ec40991e5fd76c1e070264
Leandro Cezar Zavaski <leandro_zavaski@hotmail.com>
2026-01-10 22:01:29 -0300

    feat(api): add ProducersController with awards-interval endpoint
    - Implement GET /api/producers/awards-interval
    - Return producers with min and max intervals
    - Add logging for requests

---
+++ src/GoldenRaspberryAwards.API/Controllers/ProducersController.cs
@@ -0,0 +1,25 @@
+﻿using GoldenRaspberryAwards.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenRaspberryAwards.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProducersController(IProducerService producerService, ILogger<ProducersController> logger) : ControllerBase
+    {
+        private readonly IProducerService _producerService = producerService;
+        private readonly ILogger<ProducersController> _logger = logger;
+
+        [HttpGet("awards-interval")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAwardsInterval()
+        {
+            _logger.LogInformation("Obtendo intervalos de prêmios dos produtores");
+
+            var result = await _producerService.GetProducerIntervalsAsync();
+
+            return Ok(result);
+        }
+    }
+
+}
